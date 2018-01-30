1b9f35c1b00608acfc27f5180a8593703b7fd5a7
YasseenB <34690997+S-iNo@users.noreply.github.com>
2018-01-30 14:04:18 -0500

    Add files via upload

---
+++ Gas mpg.py
@@ -0,0 +1,19 @@
+'''
+build a calculator that provides the amount of miles per gallon
+
+miles per gallon = miles driven/gallons used
+'''
+
+print("The program calculates mpg.")
+miles_driven = float(input("Enter miles driven: "))
+gallons_used = float(input("Enter gallons used:"))
+mpg= miles_driven / gallons_used
+
+print("Your miles per gallon is", mpg)
+
+
+
+print("Area formula is A=L*W")
+length_of_rectangle = float(input("Enter length of the rectangle: ")
+width_of_rectangle = float(input("Enter width of the rectangle: ")
+Area= length_of_rectangle * width_of_rectangle
